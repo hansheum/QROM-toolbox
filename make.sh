a21dd38b51ad9880f8e5c6db8ca30650b4651d11
Hans Heum <hansheum@gmail.com>
2025-03-04 12:36:33 +0100

    qrotb -> qrot

--- make.sh
+++ make.sh
@@ -7,3 +7,12 @@ pdflatex $file_name.tex
 bibtex $file_name.aux
 pdflatex $file_name.tex
 pdflatex $file_name.tex
+open $file_name.pdf -a skim
+osascript -e 'delay 0.2'
+osascript -e 'tell application "System Events" to keystroke "r" using control down'
+osascript -e 'delay 0.1'
+osascript -e 'tell application "System Events" to keystroke return'
+osascript -e 'delay 0.1'
+osascript -e 'tell application "iTerm" to activate'
+osascript -e 'delay 0.1'
+osascript -e 'tell application "System Events" to key code 123 using {command down, option down}'
