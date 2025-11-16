539cbbdcaea866ab577d2bfcbc47e96ff7dad529
hansheum <hansheum@gmail.com>
2025-11-16 21:54:55 +0100

    tweaks to CRO chapter

--- make.sh
+++ make.sh
@@ -7,5 +7,4 @@ pdflatex $file_name.tex
 bibtex $file_name.aux
 pdflatex $file_name.tex
 # Start continuous compilation (dotfiles/.latexmkrc tells latexmk to use Skim)
-#latexmk -pdf -pvc -interaction=nonstopmode -synctex=1 $file_name.tex
-#bugged? keeps looping
+latexmk -pdf -pvc -interaction=nonstopmode -synctex=1 $file_name.tex
