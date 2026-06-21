a84c545660a94ce91d4ff6a52bcdd99645e0a74d
Hans Heum <hansheum@gmail.com>
2026-06-21 20:13:55 +0200

    update to makefile

--- make.sh
+++ make.sh
@@ -6,5 +6,16 @@ rm -v *.aux
 pdflatex $file_name.tex
 bibtex $file_name.aux
 pdflatex $file_name.tex
-# Start continuous compilation (dotfiles/.latexmkrc tells latexmk to use Skim)
-latexmk -pdf -pvc -interaction=nonstopmode -synctex=1 $file_name.tex
+
+case "$(uname -s)" in
+  Darwin)
+    # macOS: start continuous compilation (dotfiles/.latexmkrc tells latexmk to use Skim)
+    latexmk -pdf -pvc -interaction=nonstopmode -synctex=1 $file_name.tex
+    ;;
+  *)
+    # CLI-only (e.g. Debian): finalize the build, then preview page 1 inline via imgcat
+    latexmk -pdf -interaction=nonstopmode -synctex=1 $file_name.tex
+    pdftoppm -png -singlefile -f 1 -l 1 "$file_name.pdf" /tmp/$file_name-pg \
+      && imgcat /tmp/$file_name-pg.png
+    ;;
+esac
