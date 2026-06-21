9c54c8e7ca2d3d5a2c402cde926bd5eb45db7760
Hans Heum <hansheum@gmail.com>
2026-06-21 20:22:39 +0200

    updated make.sh again

--- make.sh
+++ make.sh
@@ -15,7 +15,8 @@ case "$(uname -s)" in
   *)
     # CLI-only (e.g. Debian): finalize the build, then preview page 1 inline via imgcat
     latexmk -pdf -interaction=nonstopmode -synctex=1 $file_name.tex
-    pdftoppm -png -singlefile -f 1 -l 1 "$file_name.pdf" /tmp/$file_name-pg \
-      && imgcat /tmp/$file_name-pg.png
-    ;;
+    ## Image previewing doesn't work on my iOS setup
+    # pdftoppm -png -singlefile -f 1 -l 1 "$file_name.pdf" /tmp/$file_name-pg \
+    #   && imgcat /tmp/$file_name-pg.png
+    # ;;
 esac
